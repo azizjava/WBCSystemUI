170e8a1f3b7e657861bb54d1e66a0ce09b8e71bf
bhaskar.Garikapati <baschow2009@gmail.com>
2022-10-08 00:12:19 +0530

    Vehicles list code changes

--- src/app/vehicles/vehicles.component.ts
+++ src/app/vehicles/vehicles.component.ts
@@ -1,51 +1,61 @@
-
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { TranslateService } from '@ngx-translate/core';
 import { ConfirmDialogComponent } from '../common/confirm-dialog/confirm-dialog.component';
-import { GlobalConstants } from '../common/global-constants';
 import { modelDialog, tableOperation, Vehicle } from '../models';
-import { AuthenticationService } from '../services';
+import { AlertService } from '../services';
 
 import { VehicleDataComponent } from './vehicledata/vehicledata.component';
+import { VehiclesService } from './vehicles.service';
 
 @Component({
   selector: 'app-vehicles',
   templateUrl: './vehicles.component.html',
-  styleUrls: ['./vehicles.component.scss']
+  styleUrls: ['./vehicles.component.scss'],
 })
 export class VehiclesComponent implements OnInit {
-
-
-  tblColumns: string[] = ['PlateNo', 'Type', 'TransporterCode', 'TransporterName', 'Actions'];
-  tableData: any = [];
+  tblColumns: string[] = [
+    'plateNo',
+    'vehicleType',
+    'transporterCode',
+    'transporterName',
+    'Actions',
+  ];
+  public tableData: any;
+  public vehiclesData: Vehicle[] = [];
 
   public searchInput: string = '';
   public staticText: any = {};
   public actionName: string = '';
-  public sortColumn = { name: 'PlateNo', dir: 'asc' };
-  public visibleColumns = ['PlateNo', 'Type', 'Actions'];
+  public sortColumn = { name: 'plateNo', dir: 'asc' };
+  public visibleColumns = ['plateNo', 'vehicleType', 'Actions'];
 
-
-  constructor(private translate: TranslateService, private matDialog: MatDialog) { }
+  constructor(
+    private translate: TranslateService,
+    private matDialog: MatDialog,
+    private httpService: VehiclesService,
+    private alertService: AlertService
+  ) {}
 
   ngOnInit(): void {
     this.getTranslatedText();
-    this.getData();
+    this.getAllVehicles();
   }
-
-
 
   selectedRecord(actionData: tableOperation): void {
     this.actionName = actionData.action;
 
-    const dialogData = { actionName: this.actionName, headerText: 'Information', data: actionData.data };
+    const dialogData = {
+      actionName: this.actionName,
+      headerText: 'Information',
+      data: actionData.data,
+    };
 
     if (this.actionName === 'delete') {
       this.deleteDialog(dialogData);
+    } else {
+      this.openDialog(dialogData);
     }
-    else { this.openDialog(dialogData); }
-
   }
 
   searchValueChanged(value: string) {
@@ -53,7 +63,6 @@ export class VehiclesComponent implements OnInit {
   }
 
   private openDialog(dialogData: modelDialog): void {
-
     const dialogConfig = new MatDialogConfig();
 
     dialogConfig.data = dialogData;
@@ -64,151 +73,69 @@ export class VehiclesComponent implements OnInit {
 
     const dialogRef = this.matDialog.open(VehicleDataComponent, dialogConfig);
 
-    dialogRef.afterClosed().subscribe(result => {
-      if (this.actionName === "edit") {
-        const selRecord: Vehicle = {
-          Id: dialogData.data?.Id, PlateNo: result.plateNo, Type: result.type, TransporterCode: result.transporterCode, TransporterName: result.transporterName };
-        this._updateRecord(selRecord);
+    dialogRef.afterClosed().subscribe((result) => {
+      if (this.actionName === 'edit') {
+        console.log('Updated Record !!', result.plateNo);
+        this.alertService.success(`${result.plateNo} updated successfully`);
+      } else if (this.actionName === 'add') {
+        console.log('New Record !!', result.plateNo);
+        this.alertService.success(`${result.plateNo} inserted successfully`);
       }
-      else if (this.actionName === "add") { 
-        const selRecord: Vehicle = {
-          Id: GlobalConstants.commonFunction.getNewUniqueId(this.tableData), PlateNo: result.plateNo, Type: result.type, TransporterCode: result.transporterCode, TransporterName: result.transporterName };
-        this._addRecord(selRecord); 
-      }
-    });
 
+      this.getAllVehicles();
+    });
   }
 
   private deleteDialog(dialogData: modelDialog): void {
-
     const dialogConfig = new MatDialogConfig();
 
     dialogConfig.data = dialogData;
-    dialogConfig.data.message = "Are you sure you want to delete this item ?";
+    dialogConfig.data.message = 'Are you sure you want to delete this item ?';
     dialogConfig.disableClose = false;
     dialogConfig.autoFocus = true;
 
     const dialogRef = this.matDialog.open(ConfirmDialogComponent, dialogConfig);
 
-    dialogRef.afterClosed().subscribe(result => {
+    dialogRef.afterClosed().subscribe((result) => {
       this._deleteRecord(dialogData.data);
     });
-
   }
 
   private _deleteRecord(selRecord: Vehicle) {
-    console.log('Deleted Record !!', selRecord);
-    const selIndex = this.tableData.findIndex((i: any) => i.Id === selRecord.Id);
-    if (this.tableData[selIndex]) {
-      this.tableData.splice(selIndex, 1);
-      this.tableData = [...this.tableData];
-    }
-  }
-
-  private _addRecord(selRecord: Vehicle) {
-    console.log('New Record !!', selRecord);
-    this.tableData.push(selRecord);  //add the new model object to the dataSource
-    this.tableData = [...this.tableData];  //refresh the dataSource
-  }
-
-  private _updateRecord(selRecord: Vehicle) {
-    console.log('Updated Record !!', selRecord);
-    const selIndex = this.tableData.findIndex((i: any) => i.Id === selRecord.Id);
-    if (this.tableData[selIndex]) {
-      this.tableData[selIndex].PlateNo = selRecord.PlateNo;
-      this.tableData[selIndex].Type = selRecord.Type;
-      this.tableData[selIndex].TransporterCode = selRecord.TransporterCode;
-      this.tableData[selIndex].TransporterName = selRecord.TransporterName;
-    }
-  }
-
-
-  private getTranslatedText(): void {
-
-    this.translate.get(['']).subscribe((translated: string) => {
-
-      this.staticText = {
-        searchPlaceholder: this.translate.instant('placeholder.searchvehicles')
-      }
-
+    this.httpService.deleteVehicle(selRecord.plateNo).subscribe({
+      next: (res) => {
+        console.log('Deleted Record !!', selRecord);
+        this.getAllVehicles();
+      },
+      error: (e) => console.error(e),
     });
   }
 
+  private getTranslatedText(): void {
+    this.translate.get(['']).subscribe((translated: string) => {
+      this.staticText = {
+        searchPlaceholder: this.translate.instant('placeholder.searchvehicles'),
+      };
+    });
+  }
 
-  private getData(): void {
+  private getAllVehicles(): void {
+    this.httpService.getAllVehicles().subscribe({
+      next: (data: Vehicle[]) => {
+        this.vehiclesData = data;
 
-    this.tableData = [
-      {
-        "Id": "1",
-        "PlateNo": "V-01",
-        "Type": "Vehicle-01",
-        "TransporterCode": "T-01",
-        "TransporterName": "Transporter-01"
+        this.tableData = data.map((data: Vehicle) => ({
+          plateNo: data.plateNo,
+          vehicleType: data.vehicleType,
+          transporterCode: data?.transporters?.transporterCode,
+          transporterName: data?.transporters?.nameOfTransporter,
+        }));
+        
       },
-      {
-        "Id": "2",
-        "PlateNo": "V-02",
-        "Type": "Vehicle-02",
-        "TransporterCode": "T-02",
-        "TransporterName": "Transporter-02"
+      error: (error: string) => {
+        console.log(error);
+        this.alertService.error(error);
       },
-      {
-        "Id": "3",
-        "PlateNo": "V-03",
-        "Type": "Vehicle-03",
-        "TransporterCode": "T-03",
-        "TransporterName": "Transporter-03"
-      },
-      {
-        "Id": "4",
-        "PlateNo": "V-04",
-        "Type": "Vehicle-04",
-        "TransporterCode": "T-04",
-        "TransporterName": "Transporter-04"
-      },
-
-      {
-        "Id": "5",
-        "PlateNo": "V-05",
-        "Type": "Vehicle-05",
-        "TransporterCode": "T-05",
-        "TransporterName": "Transporter-05"
-      },
-      {
-        "Id": "6",
-        "PlateNo": "V-06",
-        "Type": "Vehicle-06",
-        "TransporterCode": "T-06",
-        "TransporterName": "Transporter-06"
-      },
-      {
-        "Id": "7",
-        "PlateNo": "V-07",
-        "Type": "Vehicle-07",
-        "TransporterCode": "T-07",
-        "TransporterName": "Transporter-07"
-      },
-      {
-        "Id": "8",
-        "PlateNo": "V-08",
-        "Type": "Vehicle-08",
-        "TransporterCode": "T-01",
-        "TransporterName": "Transporter-01"
-      },
-      {
-        "Id": "9",
-        "PlateNo": "V-09",
-        "Type": "Vehicle-09",
-        "TransporterCode": "T-02",
-        "TransporterName": "Transporter-02"
-      },
-      {
-        "Id": "10",
-        "PlateNo": "V-10",
-        "Type": "Vehicle-10",
-        "TransporterCode": "T-03",
-        "TransporterName": "Transporter-03"
-      },
-    ]
+    });
   }
 }
