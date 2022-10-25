ff9068b3f9656aa4eb28099a9df8d40dbedc719c
bhaskar.Garikapati <baschow2009@gmail.com>
2022-10-25 13:07:55 +0530

    Exit screen UI Changes

--- src/app/transactions/transactiondata/exitdata/exitdata.component.ts
+++ src/app/transactions/transactiondata/exitdata/exitdata.component.ts
@@ -1,18 +1,22 @@
 import { Component, Inject, OnInit } from '@angular/core';
-import { UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
+import {
+  UntypedFormBuilder,
+  UntypedFormGroup,
+  Validators,
+} from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { ActivatedRoute, Router } from '@angular/router';
 import { GlobalConstants } from 'src/app/common';
 import { findInvalidControls } from 'src/app/helper';
 import { modelDialog, Transporter } from 'src/app/models';
-
+import { AuthenticationService } from 'src/app/services';
 
 @Component({
   selector: 'app-transactionexitdata',
   templateUrl: './exitData.component.html',
-  styleUrls: ['./exitData.component.scss']
+  styleUrls: ['./exitData.component.scss'],
 })
 export class exitDataComponent implements OnInit {
-
   exitForm: UntypedFormGroup;
   vehicleList: any = [];
   transportersList: any = [];
@@ -20,32 +24,74 @@ export class exitDataComponent implements OnInit {
   productsList: any = [];
   operatorIDList: any = [];
   nationalityList: any = [];
+  keyValueData: any = [];
+  emptyKeyValue: boolean = false;
 
-  constructor(private _formBuilder: UntypedFormBuilder) { }
+  constructor(
+    private _formBuilder: UntypedFormBuilder,
+    private authenticationService: AuthenticationService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    this.exitForm = this._formBuilder.group({
+      sequenceNo: [0, [Validators.required, Validators.maxLength(50)]],
+      secondWeight: ['', [Validators.required, Validators.maxLength(50)]],
+      dateOut: [
+        {
+          value: GlobalConstants.commonFunction.getFormattedDate(),
+          disabled: true,
+        },
+      ],
+      timeOut: [
+        {
+          value: GlobalConstants.commonFunction.getFormattedTime(),
+          disabled: true,
+        },
+      ],
+      deductWeight: ['', [Validators.required, Validators.maxLength(50)]],
+      netWeight: ['', [Validators.required, Validators.maxLength(50)]],
+      priceTons: ['', [Validators.required, Validators.maxLength(50)]],
+      totalPrice: ['', [Validators.required, Validators.maxLength(50)]],
+      loginUserName: [
+        {
+          value: this.authenticationService.currentUserValue.userName,
+          disabled: true,
+        },
+      ],
+      instructions: ['', [Validators.required, Validators.maxLength(250)]],
+      deliveryNoteNo: ['', [Validators.required, Validators.maxLength(50)]],
+      orderNo: ['', [Validators.required, Validators.maxLength(50)]],
+    });
+  }
 
-    this.exitForm = this._formBuilder.group(
-      {
-        secondWeight: ['', [Validators.required, Validators.maxLength(50)]],
-        dateOut: ['', [Validators.required, Validators.maxLength(50)]],
-        timeOut: ['', [Validators.required, Validators.maxLength(50)]],
-        deductWeight: ['', [Validators.required, Validators.maxLength(50)]],
-        netWeight: ['', [Validators.required, Validators.maxLength(50)]],
-        priceTons: ['', [Validators.required, Validators.maxLength(50)]],
-        totalPrice: ['', [Validators.required, Validators.maxLength(50)]],
-        loginUserName: ['', [Validators.required, Validators.maxLength(50)]],
-        instructions: ['', [Validators.required, Validators.maxLength(250)]],
-        deliveryNoteNo: ['', [Validators.required, Validators.maxLength(50)]],
-        orderNo: ['', [Validators.required, Validators.maxLength(50)]],
-       
-      });
-  }  
-
-  save() {
+  public save() {
     // stop here if form is invalid
     if (!findInvalidControls(this.exitForm)) {
       return;
     }
+  }
+
+  public cancel(): void {
+    this.router.navigate(['/dashboard/transactions'], {
+      relativeTo: this.route,
+    });
+  }
+
+  public addKeyValues() {
+    if (this.keyValueData.length === 0) {
+      this.emptyKeyValue = false;
+      this.keyValueData.push({ key: '', value: '' });
+    } else {
+      this.emptyKeyValue = this.keyValueData.some((obj: any) => !obj.key);
+      if (!this.emptyKeyValue) {
+        this.keyValueData.push({ key: '', value: '' });
+      }
+    }
+  }
+
+  public trackByFn(index: number, item: any) {
+    return item;
   }
 }
