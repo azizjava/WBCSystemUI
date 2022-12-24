d02741d7187c8231f442cf8bc5ae026433f60bd7
bhaskar.Garikapati <baschow2009@gmail.com>
2022-12-24 17:12:49 +0530

    Weigh Bridge Weight API integration

--- src/app/weighbridgeweight/weighbridgeweight.component.ts
+++ src/app/weighbridgeweight/weighbridgeweight.component.ts
@@ -1,16 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
 import { MatRadioChange } from '@angular/material/radio';
-import { WeightBridgeWeightService } from './weighbridgeweight.service';
+import { AlertService } from '../services';
+import { WeightBridgeScaleService } from './weighbridgescale.service';
 
 @Component({
   selector: 'app-weightbridgeweight',
   templateUrl: './weighbridgeweight.component.html',
   styleUrls: ['./weighbridgeweight.component.scss'],
 })
-export class WeightbridgeWeightComponent {
-  constructor(private httpService: WeightBridgeWeightService) {}
+export class WeightbridgeWeightComponent implements OnInit {
 
-  public radioChange($event: MatRadioChange) {
-    console.log($event.source.name, $event.value);
+  scaleForm: UntypedFormGroup;
+  selectedscaleType :string ='Kg';
+
+  constructor(private httpService: WeightBridgeScaleService,
+    private _formBuilder: UntypedFormBuilder,
+    private alertService: AlertService) {}
+
+  public ngOnInit() :void {
+    this.scaleForm = this._formBuilder.group({    
+      scaleType: [this.selectedscaleType, [Validators.required]],
+    });
+
+    this.httpService.getWeightScaleType().subscribe({
+      next: (data: any) => {
+        this.selectedscaleType = data.escale;
+        this.scaleForm.controls['scaleType'].setValue(this.selectedscaleType);
+      },
+      error: (error: string) => {
+        this.alertService.error(error);
+        this.scaleForm.controls['scaleType'].setValue(this.selectedscaleType);
+      },
+    });
+  }
+
+  public radioChange($event: MatRadioChange) {    
+    const data = {
+      escale: $event.value.toString().toUpperCase()
+    }
+    this.httpService.updateWeightScaleType(data).subscribe({
+      next: (data: any) => {
+        this.alertService.success('Data updated successfully.');        
+      },
+      error: (error: string) => {
+        this.alertService.error(error);
+        this.scaleForm.controls['scaleType'].setValue(this.selectedscaleType);
+      },
+    });
   }
 }
