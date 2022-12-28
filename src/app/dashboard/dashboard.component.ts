926997715d7cc252e0f4edac9313c5c7bdef6cb3
bhaskar.Garikapati <baschow2009@gmail.com>
2022-12-28 12:47:12 +0530

    Language translation code changes

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -9,141 +9,135 @@ import { GlobalConstants, LeftMenuItem } from '../common';
 
 
 @Component({
-    selector: 'app-dashboard',
-    templateUrl: './dashboard.component.html',
-    styleUrls: ['./dashboard.component.scss']
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
+  public currentUser?: User;
+  public actionName: string = '';
+  public currentYear: number = 0;
+  public NavMenu: LeftMenuItem[] = [];
+  public selectedMenu: string = '';
 
-    public currentUser?: User;
-    public actionName: string = '';
-    public currentYear: number = 0;
-    public NavMenu: LeftMenuItem[] = [];
-    public selectedMenu: string = '';
+  private _hideHeaderText: boolean = false;
 
-    private _hideHeaderText :boolean =false;
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private authenticationService: AuthenticationService,
+    private translate: TranslateService,
+    private matDialog: MatDialog
+  ) {
+    this.authenticationService.currentUser.subscribe((x) => {
+      if (x) {
+        this.currentUser = x;
+        this.translate.setDefaultLang(x?.language || 'en');
+        translate.use(x?.language);
+      }
+    });
 
-    constructor(private router: Router,
-        private route: ActivatedRoute,
-        private authenticationService: AuthenticationService,
-        private translate: TranslateService,
-        private matDialog: MatDialog) {
+    this.router.events.subscribe((event) => {
+      if (
+        event instanceof NavigationEnd &&
+        this.router.url.includes('transactions/')
+      ) {
+        this.selectedMenu = '';
+        this._hideHeaderText = true;
+      } else {
+        this._hideHeaderText = false;
+      }
+    });
+  }
 
-        this.authenticationService.currentUser.subscribe(x => {
-            if (x) {
-                this.currentUser = x;
-                this.translate.setDefaultLang(x?.language || 'en');
-                translate.use(x?.language);
-            }
-
-        });
-
-        this.router.events.subscribe(event => {
-            if (event instanceof NavigationEnd && this.router.url.includes('transactions/')) {        
-                this.selectedMenu = "";
-                this._hideHeaderText =true;             
-            }
-            else{
-                this._hideHeaderText =false;
-            }
-           
-        });
-
+  public ngOnInit(): void {
+    const sidebarToggle = document.body.querySelector('#sidebarToggle');
+    if (sidebarToggle) {
+      sidebarToggle.addEventListener('click', (event) => {
+        event.preventDefault();
+        document
+          .getElementById('mainContainer')
+          ?.classList.toggle('sb-sidenav-toggled');
+      });
     }
 
-    public ngOnInit(): void {
-        const sidebarToggle = document.body.querySelector('#sidebarToggle');
-        if (sidebarToggle) {
-            sidebarToggle.addEventListener('click', event => {
-                event.preventDefault();
-                document.getElementById('mainContainer')?.classList.toggle('sb-sidenav-toggled');
-            });
-        }        
+    this.currentYear = GlobalConstants.commonFunction.getCurrentYear();
 
-        this.currentYear = GlobalConstants.commonFunction.getCurrentYear();
+    this.getMenuItemsList();
+  }
 
-        this.getMenuItemsList();
+  public logout() {
+    this.authenticationService.logout();
+    this.router.navigate(['/login']);
+  }
+
+  public changePassword() {
+    this.actionName = 'changePwd';
+
+    const dialogData = {
+      data: null,
+      actionName: this.actionName,
+      headerText: 'Change Password',
+    };
+
+    this.openDialog(dialogData);
+  }
+
+  public viewProfile() {
+    this.actionName = 'viewProfile';
+
+    const dialogData = {
+      actionName: this.actionName,
+      headerText: 'Information',
+      data: this.currentUser,
+    };
+
+    this.openDialog(dialogData);
+  }
+
+  public trackByFn(index: number, item: LeftMenuItem) {
+    return item;
+  }
+
+  public navigateToRoute(menuItem: LeftMenuItem): void {
+    if (menuItem.routePath) {
+      this.selectedMenu = menuItem.menuName;
+      this.router.navigate([menuItem.routePath], { relativeTo: this.route });
     }
+  }
 
+  public isActive(routeName: any): boolean {
+    return this.router.url.includes(`/${routeName}`);
+  }
 
-    public logout() {
-        this.authenticationService.logout();
-        this.router.navigate(['/login']);
-    }
+  private openDialog(dialogData: modelDialog): void {
+    const dialogConfig = new MatDialogConfig();
 
+    dialogConfig.data = dialogData;
 
-    public changePassword() {
+    dialogConfig.disableClose = true;
+    dialogConfig.autoFocus = true;
 
-        this.actionName = 'changePwd';
+    const dialogRef = this.matDialog.open(ModaldialogComponent, dialogConfig);
 
-        const dialogData = { data: null, actionName: this.actionName, headerText: 'Change Password' };
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log('The dialog was closed', result);
+    });
+  }
 
-        this.openDialog(dialogData);
-    }
+  private getMenuItemsList(): void {
+    const getLeftNavMenuItemsList =
+      GlobalConstants.commonFunction.getLeftNavMenuItemsList();
 
-    public viewProfile() {
+    getLeftNavMenuItemsList.forEach((element: LeftMenuItem) => {
+      this.NavMenu.push(element);
+    });
 
-        this.actionName = 'viewProfile';
-
-        const dialogData = { actionName: this.actionName, headerText: 'Information', data: this.currentUser };
-
-        this.openDialog(dialogData);
-    }
-
-    public trackByFn(index: number, item: LeftMenuItem) {
-        return item;
-    }
-
-    public navigateToRoute(menuItem: LeftMenuItem): void {
-
-        if (menuItem.routePath) {
-            this.selectedMenu = menuItem.menuName;
-            this.router.navigate([menuItem.routePath], { relativeTo: this.route });
-        }
-    }
-
-    public isActive(routeName: any): boolean {
-        return this.router.url.includes(`/${routeName}`);
-    }
-
-
-
-    private openDialog(dialogData: modelDialog): void {
-
-        const dialogConfig = new MatDialogConfig();
-
-        dialogConfig.data = dialogData;
-
-        dialogConfig.disableClose = true;
-        dialogConfig.autoFocus = true;
-
-
-        const dialogRef = this.matDialog.open(ModaldialogComponent, dialogConfig);
-
-        dialogRef.afterClosed().subscribe(result => {
-            console.log('The dialog was closed', result);
-        });
-
-
-    }
-
-    private getMenuItemsList(): void {
-
-        const getLeftNavMenuItemsList = GlobalConstants.commonFunction.getLeftNavMenuItemsList();
-
-        this.translate.get(['']).subscribe(translations => {
-
-            getLeftNavMenuItemsList.forEach((element: LeftMenuItem) => {
-                element.menuName = this.translate.instant('navleftmenuitemslist.' + element.menuName);
-                this.NavMenu.push(element);
-            });
-
-            this.selectedMenu = !this._hideHeaderText && this.NavMenu.filter(x => (x.routePath && this.router.url.includes(x.routePath)))[0]?.menuName || "";
-        });
-
-
-    }
-
-
-
+    this.selectedMenu =
+      (!this._hideHeaderText &&
+        this.NavMenu.filter(
+          (x) => x.routePath && this.router.url.includes(x.routePath)
+        )[0]?.menuName) ||
+      '';
+  }
 }
