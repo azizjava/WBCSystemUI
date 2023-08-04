027cdd915c80d9cc98f05b25f56e6139543c8f6b
bhaskar.Garikapati <baschow2009@gmail.com>
2023-08-04 21:03:42 +0530

    Added Carousel control

--- src/app/transactions/transactiondata/viewtransaction/viewtransaction.component.ts
+++ src/app/transactions/transactiondata/viewtransaction/viewtransaction.component.ts
@@ -1,20 +1,56 @@
 import { ComponentType } from '@angular/cdk/portal';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { TransactionsService } from '../../transactions.service';
+import { AlertService } from 'src/app/services';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 
 
 @Component({
   selector: 'app-viewtransaction',
   templateUrl: './viewtransaction.component.html',
   styleUrls: ['./viewtransaction.component.scss'],
+  providers: [NgbCarouselConfig]
 })
+
 export class viewTransactionComponent implements OnInit {
-  
-  public constructor() {
-    
+  public transactionData:any;
+  images :any;
+
+  constructor(
+    private route: ActivatedRoute,
+    private httpService: TransactionsService,
+    private alertService: AlertService,
+    config: NgbCarouselConfig
+  ) {
+    // customize default values of carousels used by this component tree
+		config.interval = 1000;
+		config.wrap = true;
+		config.keyboard = false;
+		config.pauseOnHover = true;
   }
 
-  public ngOnInit(): void { 
-   console.log()
+  public ngOnInit(): void {
+    this.route.paramMap.subscribe((params) => {
+      const sequenceNo = params.get('id') || '';
+      
+      sequenceNo && this._getTransactionById(sequenceNo);
+    });
+
+    this.images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
   }
+  
+  private _getTransactionById(sequenceNo: string): void {
+    this.httpService.getTransactionById(sequenceNo).subscribe({
+      next: (data: any) => {
+       this.transactionData = data;  
+       
+      },
+      error: (error) => {
+        this.alertService.error(error);
+      },
+    });
+  } 
+
 }
 
