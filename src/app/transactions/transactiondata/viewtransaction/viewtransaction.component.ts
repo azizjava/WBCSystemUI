bd4fd69a8572834258d3b7472566f4342b6a2e4a
bhaskar.Garikapati <baschow2009@gmail.com>
2023-08-10 21:32:53 +0530

    Added slider control

--- src/app/transactions/transactiondata/viewtransaction/viewtransaction.component.ts
+++ src/app/transactions/transactiondata/viewtransaction/viewtransaction.component.ts
@@ -3,14 +3,13 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { TransactionsService } from '../../transactions.service';
 import { AlertService } from 'src/app/services';
-import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
+import { NgxGalleryOptions, NgxGalleryImage, NgxGalleryAnimation } from '@kolkov/ngx-gallery';
 
 
 @Component({
   selector: 'app-viewtransaction',
   templateUrl: './viewtransaction.component.html',
   styleUrls: ['./viewtransaction.component.scss'],
-  providers: [NgbCarouselConfig]
 })
 
 export class viewTransactionComponent implements OnInit {
@@ -19,17 +18,42 @@ export class viewTransactionComponent implements OnInit {
   public entryData:any;
   public exitData:any;
 
+  galleryOptions: NgxGalleryOptions[];
+  galleryImages: NgxGalleryImage[];
+
   constructor(
     private route: ActivatedRoute,
     private httpService: TransactionsService,
     private alertService: AlertService,
-    config: NgbCarouselConfig
   ) {
-    // customize default values of carousels used by this component tree
-		config.interval = 1000;
-		config.wrap = true;
-		config.keyboard = false;
-		config.pauseOnHover = true;
+		
+    this.galleryOptions = [
+      {
+          width: '100%',
+          height: '300px',
+          thumbnailsColumns: 4,
+          imageAnimation: NgxGalleryAnimation.Slide,
+          imageAutoPlay: true,
+          fullWidth:false,
+          previewFullscreen:false,
+          preview:false
+      },
+      {
+          breakpoint: 800,
+          width: '100%',
+          height: '300px',
+          imagePercent: 80,
+          thumbnailsPercent: 20,
+          thumbnailsMargin: 20,
+          thumbnailMargin: 20
+      },
+      // max-width 400
+      {
+          breakpoint: 400,
+          preview: false,
+      },
+      
+    ]
   }
 
   public ngOnInit(): void {
@@ -40,6 +64,23 @@ export class viewTransactionComponent implements OnInit {
     });
 
     this.images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
+
+    this.galleryImages = [
+      {
+          small: this.images[0],
+          medium: this.images[0],
+          big: this.images[0],
+          
+      },
+      {
+        small: this.images[1],
+        medium: this.images[1],
+      },
+      {
+        small: this.images[2],
+        medium: this.images[2],
+      }
+  ];
   }
   
   private _getTransactionById(sequenceNo: string): void {
--- src/app/transactions/transactions.module.ts
+++ src/app/transactions/transactions.module.ts
@@ -13,9 +13,9 @@ import { TransactionDataComponent } from './transactiondata/transactiondata.comp
 import { entryDataComponent } from './transactiondata/entrydata/entrydata.component';
 import { exitDataComponent } from './transactiondata/exitdata/exitdata.component';
 import { weightBridgeComponent } from './weightbridge/weightbridge.component';
-import { UiSwitchModule } from 'ngx-ui-switch';
 import { DriverDataComponent } from './driverdata/driverdata.component';
 import { viewTransactionComponent } from './transactiondata/viewtransaction/viewtransaction.component';
+import { NgxGalleryModule } from '@kolkov/ngx-gallery';
 
 export function createTranslateLoader(http: HttpClient) {
   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
@@ -37,17 +37,9 @@ export function createTranslateLoader(http: HttpClient) {
     MaterialModule,
     ReactiveFormsModule,
     FormsModule,
+    NgxGalleryModule,
     TransactionsRoutingModule,
-    SharedcomponentsModule,
-    UiSwitchModule.forRoot({
-      size: 'medium',
-      color: '#0d6efd',
-      switchColor: '#FFFFFF',
-      defaultBgColor: '#ca2222',
-      defaultBoColor : '#476EFF',
-      checkedLabel: 'Connected',
-      uncheckedLabel: 'Disconnected'
-    }),
+    SharedcomponentsModule,  
     TranslateModule.forChild({
       loader: { provide: TranslateLoader, useFactory: createTranslateLoader, deps: [HttpClient] },
       isolate: false,
