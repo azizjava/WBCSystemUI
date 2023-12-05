3e81a3306050a8c1983bbb864b2be16f7cca0685
bhaskar.Garikapati <baschow2009@gmail.com>
2023-12-05 22:10:37 +0530

    Added ew template code changes

--- src/app/template/templatedata/templatedata.component.ts
+++ src/app/template/templatedata/templatedata.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Inject, OnInit } from '@angular/core';
 import {
   UntypedFormBuilder,
   UntypedFormGroup,
@@ -11,76 +11,209 @@ import { modelDialog, Nationality } from 'src/app/models';
 import { AlertService, AuthenticationService } from 'src/app/services';
 import { TemplateService } from '../template.service';
 
+declare var $: any; // declaring jquery in this way solved the problem
+
 @Component({
   selector: 'app-templatedata',
   templateUrl: './templatedata.component.html',
   styleUrls: ['./templatedata.component.scss'],
 })
-export class TemplateDataComponent implements OnInit {
-  public form: UntypedFormGroup;
-  public recordData!: Nationality;
+export class TemplateDataComponent implements OnInit, AfterViewInit {
   public staticText: any = {};
+  public count = 0;
 
-  private _hasChange: boolean = false;
+  constructor(private translate: TranslateService) {}
 
-  row = [
-    {
-      id : '',
-      name: '',
-      email: ''
-    },
-    {
-      id : '',
-      name: '',
-      email: ''
-    },
-    {
-      id : '',
-      name: '',
-      email: ''
-    }
-  ];
-  
-  
-
-  constructor(
-    private translate: TranslateService,
-  ) {}
-
-  ngOnInit(): void {  
+  ngOnInit(): void {
     this._getTranslatedText();
   }
 
-  addTable() {
-    const obj = {
-      id: '',
-      name: '',
-      email: ''
-    }
-    this.row.push(obj)
-  }
-  
-  deleteRow(x: number){
-    var delBtn = confirm(" Do you want to delete ?");
-    if ( delBtn == true ) {
-      this.row.splice(x, 1 );
-    }   
-  } 
-
-  close() {
+  // Function to make elements draggable
+  makeDraggable(element: any) {
+    element.draggable({
+      revert: 'invalid',
+      appendTo: 'body',
+      helper: 'clone',
+    });
   }
 
-  save() {
-  
+  addDraggable() {
+    this.count++;
+
+    const draggableItem = $(`<div class="draggable" id="item${this.count}">
+        <div class="card" style="height: 100%;">
+        <div class="card-header" style="height: 2.5rem;">          
+        </div>
+        <div class="card-body">          
+          
+        </div>
+      </div>
+        </div>`);
+    $('#panel-1').append(draggableItem);
+    this.makeDraggable(draggableItem);
+  }
+
+  ngAfterViewInit() {
+    $(document).ready(() => {
+      var activeElement: any;
+      Array.from(document.getElementsByClassName('predefined-label')).forEach(
+        (element) => {
+          this.makeDraggable($(element));
+        }
+      );
+
+      // Droppable functionality for panel-2
+      $('#panel-2').droppable({
+        accept: '.draggable, .predefined-label',
+        drop: function (event: any, ui: any) {
+          if (!ui.draggable.hasClass('dropped-in-panel2')) {
+            const clone = $(ui.helper).clone();
+            clone
+              .removeClass('ui-draggable')
+              .addClass('dropped-in-panel2')
+              .css({
+                position: 'absolute',
+                top: ui.position.top - $(this).offset().top,
+                left: ui.position.left - $(this).offset().left,
+              })
+              .appendTo(this)
+              .draggable({
+                containment: 'parent',
+              })
+              .resizable();
+
+            clone.on('click', (event: any) => {
+              $('.selected').removeClass('selected');
+              $(event.target).addClass('selected');
+              activeElement = $(event.target);
+            });
+
+            if (ui.draggable.hasClass('draggable')) {
+              ui.draggable.remove();
+            }
+          }
+        },
+      });
+
+      // Clear functionality for panel-2
+      $('#clearBtn').on('click', function () {
+        $('#panel-2 .dropped-in-panel2').remove();
+      });
+
+      // Load functionality
+      $('#loadFile').on('change', (e: any) => {
+        const file = e.target.files[0];
+        if (file) {
+          const reader = new FileReader();
+          reader.onload = (event: any) => {
+            try {
+              const componentsData = JSON.parse(event.target.result);
+              $('#panel-2 .dropped-in-panel2').remove();
+              componentsData.forEach((data: any) => {
+                let $newComponent;
+                if (data.type === 'label') {
+                  $newComponent = $(
+                    '<div class="predefined-label dropped-in-panel2"></div>'
+                  ).text(data.text);
+                } else {
+                  $newComponent = $(
+                    '<div class="draggable dropped-in-panel2"></div>'
+                  ).text(data.text);
+
+                  $(`<div class="card" style="height: 100%;">
+                  <div class="card-header" style="height: 2.5rem;"></div>
+                  <div class="card-body"></div>
+                  </div>`).appendTo($newComponent);
+                }
+
+                $newComponent
+                  .css({
+                    top: data.top,
+                    left: data.left,
+                    width: data.width,
+                    height: data.height,
+                    background: data.background,
+                    border: data.border,
+                    position: 'absolute',
+                  })
+                  .appendTo('#panel-2');
+
+                $newComponent.draggable({ containment: 'parent' }).resizable();
+
+                $newComponent.on('click', (event: any) => {
+                  $('.selected').removeClass('selected');
+                  $(event.target).addClass('selected');
+                  activeElement = $(event.target);
+                });
+              });
+            } catch (error) {
+              console.error('Error parsing JSON:', error);
+            }
+          };
+          reader.onerror = function (error) {
+            console.error('Error reading file:', error);
+          };
+          reader.readAsText(file);
+        }
+      });
+
+      $('#setA4').click(function () {
+        $('#panel-2').removeClass('a2-size').addClass('a4-size');
+      });
+
+      $('#setA2').click(function () {
+        $('#panel-2').removeClass('a4-size').addClass('a2-size');
+      });
+
+      // Delete functionality for selected component
+      $('#deleteBtn').on('click', () => {
+        console.log(activeElement);
+        if (activeElement && activeElement.parent().is('#panel-2')) {
+          activeElement.remove();
+          activeElement = null;
+          $('.selected').removeClass('selected');
+        } else {
+          alert('Please select a component in panel 2 to delete it.');
+        }
+      });
+    });
+  }
+
+  public saveTemplate() {
+    const componentsData: any = [];
+    Array.from($('#panel-2 .dropped-in-panel2')).forEach((element: any) => {
+      console.log('$(this)', element);
+      const $this = $(element);
+      componentsData.push({
+        id: $this.attr('id'),
+        type: $this.hasClass('predefined-label') ? 'label' : 'component',
+        text: $this.text(),
+        top: $this.css('top'),
+        left: $this.css('left'),
+        width: $this.css('width'),
+        height: $this.css('height'),
+      });
+    });
+
+    const blob = new Blob([JSON.stringify(componentsData)], {
+      type: 'application/json',
+    });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'componentsData.json';
+    document.body.appendChild(a);
+    a.click();
+    window.URL.revokeObjectURL(url);
   }
 
   private _getTranslatedText(): void {
     this.translate.get(['']).subscribe((translated: string) => {
       this.staticText = {
-        nationality: this.translate.instant('nationality.tbl_header.drivernationality'),
+        nationality: this.translate.instant(
+          'nationality.tbl_header.drivernationality'
+        ),
       };
     });
   }
-
- 
 }
