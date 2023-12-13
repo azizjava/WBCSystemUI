b1a5520ddfbc0ddd5caca89af2198c04691f9e36
bhaskar.Garikapati <baschow2009@gmail.com>
2023-12-13 19:49:34 +0530

    Code changes for making panel 2 resizable

--- src/app/template/templatedata/templatedata.component.ts
+++ src/app/template/templatedata/templatedata.component.ts
@@ -1,10 +1,22 @@
-import { AfterViewInit, Component, Inject, OnInit, TemplateRef, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  Inject,
+  OnInit,
+  TemplateRef,
+  ViewChild,
+} from '@angular/core';
 import {
   UntypedFormBuilder,
   UntypedFormGroup,
   Validators,
 } from '@angular/forms';
-import { MatDialogRef, MAT_DIALOG_DATA, MatDialogConfig, MatDialog } from '@angular/material/dialog';
+import {
+  MatDialogRef,
+  MAT_DIALOG_DATA,
+  MatDialogConfig,
+  MatDialog,
+} from '@angular/material/dialog';
 import { TranslateService } from '@ngx-translate/core';
 import { findInvalidControls } from 'src/app/helper';
 import { modelDialog, Nationality } from 'src/app/models';
@@ -22,16 +34,21 @@ declare var $: any; // declaring jquery in this way solved the problem
 export class TemplateDataComponent implements OnInit, AfterViewInit {
   public staticText: any = {};
   public count = 0;
-  public labelNames:any[] = [];
-  public labelValues:any[] = [];
+  public labelNames: any[] = [];
+  public labelValues: any[] = [];
   @ViewChild('myLabelDialog') labelChangeDialog = {} as TemplateRef<any>;
 
-  constructor(private translate: TranslateService, private matDialog: MatDialog) {}
+  constructor(
+    private translate: TranslateService,
+    private matDialog: MatDialog
+  ) {}
 
   ngOnInit(): void {
     this._getTranslatedText();
-    this.labelNames = GlobalConstants.commonFunction.getTemplateLabelNamesList();
-    this.labelValues = GlobalConstants.commonFunction.getTemplateLabelValuesList();
+    this.labelNames =
+      GlobalConstants.commonFunction.getTemplateLabelNamesList();
+    this.labelValues =
+      GlobalConstants.commonFunction.getTemplateLabelValuesList();
   }
 
   // Function to make elements draggable
@@ -59,14 +76,14 @@ export class TemplateDataComponent implements OnInit, AfterViewInit {
     this.makeDraggable(draggableItem);
   }
 
-  editLabelName(label:any, index:number) {
+  editLabelName(label: any, index: number) {
     const dialogData = {
       headerText: 'Information',
       data: label.value,
-      labelInfo:label,
-      index:index
+      labelInfo: label,
+      index: index,
     };
-    this.openDialog(dialogData)
+    this.openDialog(dialogData);
   }
 
   ngAfterViewInit() {
@@ -77,6 +94,8 @@ export class TemplateDataComponent implements OnInit, AfterViewInit {
           this.makeDraggable($(element));
         }
       );
+
+      $('#panel-2').resizable();
 
       // Droppable functionality for panel-2
       $('#panel-2').droppable({
@@ -123,15 +142,24 @@ export class TemplateDataComponent implements OnInit, AfterViewInit {
           const reader = new FileReader();
           reader.onload = (event: any) => {
             try {
-              const componentsData = JSON.parse(event.target.result);
+              const results = JSON.parse(event.target.result);
+              const panel2Size = results?.panel2Size;
+              if (panel2Size) {
+                $("#panel-2").css({
+                    'width': panel2Size.width,
+                    'height': panel2Size.height
+                });
+              }
+
+              const componentsData = results?.components;
               $('#panel-2 .dropped-in-panel2').remove();
               componentsData.forEach((data: any) => {
                 let $newComponent;
                 let text = data.text;
                 if (data.type === 'label') {
-                  const orgLbl = data.text.split("=");
-                  if(orgLbl.length >1 ){
-                    text = orgLbl[1];                    
+                  const orgLbl = data.text.split('=');
+                  if (orgLbl.length > 1) {
+                    text = orgLbl[1];
                   }
                   $newComponent = $(
                     '<div class="predefined-label dropped-in-panel2"></div>'
@@ -173,10 +201,10 @@ export class TemplateDataComponent implements OnInit, AfterViewInit {
           };
           reader.onerror = function (error) {
             console.error('Error reading file:', error);
-            $("#loadFile").val('');
+            $('#loadFile').val('');
           };
           reader.readAsText(file);
-          $("#loadFile").val('');
+          $('#loadFile').val('');
         }
       });
 
@@ -205,7 +233,6 @@ export class TemplateDataComponent implements OnInit, AfterViewInit {
   public saveTemplate() {
     const componentsData: any = [];
     Array.from($('#panel-2 .dropped-in-panel2')).forEach((element: any) => {
-      console.log('$(this)', element);
       const $this = $(element);
       const type = $this.hasClass('predefined-label') ? 'label' : 'component';
       let text = $this.text();
@@ -226,7 +253,18 @@ export class TemplateDataComponent implements OnInit, AfterViewInit {
       });
     });
 
-    const blob = new Blob([JSON.stringify(componentsData)], {
+    const panel2Data = {
+      width: $('#panel-2').css('width'),
+      height: $('#panel-2').css('height'),
+    };
+
+    const saveData = {
+      components: componentsData,
+      panel2Size: panel2Data,
+      // colorPickers: colorPickerStates,
+    };
+
+    const blob = new Blob([JSON.stringify(saveData)], {
       type: 'application/json',
     });
     const url = URL.createObjectURL(blob);
@@ -257,8 +295,8 @@ export class TemplateDataComponent implements OnInit, AfterViewInit {
 
     const dialogRef = this.matDialog.open(this.labelChangeDialog, dialogConfig);
 
-    dialogRef.afterClosed().subscribe((result:any) => {
-      if(result){
+    dialogRef.afterClosed().subscribe((result: any) => {
+      if (result) {
         this.labelNames[dialogData?.index].value = result;
       }
     });
