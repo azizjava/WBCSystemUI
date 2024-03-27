1d96b5b6c6d2097735e29160928b500356694a53
bhaskar.Garikapati <baschow2009@gmail.com>
2024-03-27 19:54:11 +0530

    Vehicle code changes

--- src/app/vehicles/vehicles.component.ts
+++ src/app/vehicles/vehicles.component.ts
@@ -1,18 +1,19 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { ConfirmDialogComponent } from '../common/confirm-dialog/confirm-dialog.component';
-import { modelDialog, tableOperation, Vehicle } from '../models';
+import { dateRange, modelDialog, tableOperation, transactionFilter, Vehicle } from '../models';
 import { AlertService } from '../services';
 
 import { VehicleDataComponent } from './vehicledata/vehicledata.component';
 import { VehiclesService } from './vehicles.service';
+import { Sort } from '@angular/material/sort';
 
 @Component({
   selector: 'app-vehicles',
   templateUrl: './vehicles.component.html',
   styleUrls: ['./vehicles.component.scss'],
 })
-export class VehiclesComponent implements OnInit {
+export class VehiclesComponent  {
   tblColumns: string[] = [
     'plateNo',
     'vehicleType',
@@ -28,16 +29,18 @@ export class VehiclesComponent implements OnInit {
   public actionName: string = '';
   public sortColumn = { name: 'plateNo', dir: 'asc' };
   public visibleColumns = ['plateNo', 'vehicleType', 'Actions'];
+  public dateRange: transactionFilter | null;
+  public totalItems :number = 0;
+  public plateNo : string ="";
+
+  private _pageSize :number = 10;
+  private _currentPage :number = 0;
 
   constructor(
     private matDialog: MatDialog,
     private httpService: VehiclesService,
     private alertService: AlertService
   ) {}
-
-  ngOnInit(): void {
-    this.getAllVehicles();
-  }
 
   selectedRecord(actionData: tableOperation): void {
     this.actionName = actionData.action;
@@ -114,19 +117,59 @@ export class VehiclesComponent implements OnInit {
         this.alertService.error(error); 
       },
     });
-  }  
+  }
+
+  public dateSelectionChangedEvent(dataRage: transactionFilter){
+    this.dateRange = dataRage;
+    localStorage.setItem('selDateRange', JSON.stringify(this.dateRange));
+    this.plateNo =dataRage.sequenceNo;    
+    this._currentPage = 0;
+    this.getAllVehicles();
+  }
+
+  onsequenceNoChange(plateNo: string) :void {
+    this.plateNo = plateNo;
+    this._currentPage = 0;
+    this.getAllVehicles();
+  }
+
+  public onPageOptionChange(pageOption: any) :void {
+    this._pageSize = pageOption.pageSize;
+    this._currentPage = pageOption.pageIndex;
+    this.getAllVehicles();
+  }
+
+  public onSortOptionChange(sortOption: Sort) :void {
+    if(sortOption.active !== this.sortColumn.name && sortOption.direction !== this.sortColumn.dir){
+    this.getAllVehicles();
+    }
+  }
 
   private getAllVehicles(): void {
-    this.httpService.getAllVehicles().subscribe({
-      next: (data: Vehicle[]) => {
-        this.vehiclesData = data;
+    let searchFilter:any = {
+      startDate: this.dateRange?.startDate,
+      endDate:  this.dateRange?.endDate,
+      pageNo: this._currentPage,
+      pageSize:this._pageSize,
+      sortOrder: this.sortColumn.dir,
+      sortBy:this.sortColumn.name
+     };
 
-        this.tableData = data.map((data: Vehicle) => ({
-          plateNo: data.plateNo,
-          vehicleType: data.vehicleType,
-          vehicleWeight: data.vehicleWeight,
-          transporterCode: data?.transporters?.transporterCode,
-          transporterName: data?.transporters?.transporterName,
+     if(this.plateNo){
+      searchFilter.plateNo = this.plateNo;
+    }
+
+    this.httpService.getAllPaginatedVehicles(searchFilter).subscribe({
+      next: (data: any) => {
+        this.vehiclesData = data;
+        this.totalItems= data["total-items"]; 
+
+        this.tableData = data?.listAllVehicle?.map((response: any) => ({
+          plateNo: response[0],
+          vehicleType: response[1],
+          vehicleWeight: response[2],
+          transporterCode: response[3],
+          transporterName: response[4],
         }));
       },
       error: (error: string) => {
